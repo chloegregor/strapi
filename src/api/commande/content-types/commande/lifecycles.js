92909992e8acae7b8873323ebae25076a664e0e1
chloe gregoire <chle.gregire@gmail.com>
2025-10-02 13:44:30 +0200

    adjustment mail

--- src/api/commande/content-types/commande/lifecycles.js
+++ src/api/commande/content-types/commande/lifecycles.js
@@ -46,7 +46,7 @@ module.exports = {
 
         subject: `Confirmation de votre commande`,
         text: `Bonjour ${clientName},\n\nMerci pour votre commande !\n\nAdresse: ${clientAddress}\n\nDétails de la commande:\n${commandeLines.map(line => `- ${line.name}`).join('\n')}\n\nNous vous informerons lorsqu'elle aura été expédiée.\n\nCordialement,\n Kraze`,
-        html: `<p>Bonjour <strong>${clientName}</strong>,</p><p>Merci pour votre commande !(référence : ${result.documentId}) <p>Adresse: ${clientAddress}</p><p>Détails de la commande:</p><ul>${commandeLines.map(line => `<li>${line.name}</li>`).join('')}</ul><p>Nous vous informerons lorsqu'elle aura été expédiée.</p><p>Cordialement,<br>Kraze</p>
+        html: `<p>Bonjour <strong>${clientName}</strong>,</p><p>Merci pour votre commande référence : ${result.documentId} !<p>Adresse: ${clientAddress}</p><p>Détails de la commande:</p><ul>${commandeLines.map(line => `<li>${line.name}</li>`).join('')}</ul><p>Nous vous informerons lorsqu'elle aura été expédiée.</p><p>Cordialement,<br>Kraze</p>
         `,
     })
 
